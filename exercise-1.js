2a355871a006544f66ab94c304db6b6ae0ad345c
mikeerockscode <mikeerocksvans@gmail.com>
2025-04-15 11:31:58 -0700

    ex1 and 2

--- exercise-1.js
+++ exercise-1.js
@@ -4,3 +4,29 @@ Start with exercise-1.md if you haven't read it!
 */
 
 // YOUR WORK GOES HERE //
+
+const Freddys = {
+  name: "Freddys",
+  cuisines: "american food",
+  numberOfStars: 2,
+  favorited: true,
+};
+
+Freddys.address = "my street 123";
+Freddys.zipcode = 55546;
+Freddys.acceptsReservations = true;
+
+Freddys.numberOfStars = 5;
+Freddys.favorited = false;
+Freddys.cuisines = "frozen custard";
+
+Freddys.retrieveProperty = function (property) {
+  return hasOwnProperty(property)
+    ? [property]
+    : "the information you requested does not exist";
+};
+console.log(Freddys.retrieveProperty("cuisines"));
+console.log(Freddys.retrieveProperty("favorited"));
+console.log(Freddys.retrieveProperty("name"));
+console.log(Freddys.retrieveProperty("nickname"));
+console.log(Freddys.retrieveProperty("state"));
--- exercise-2.js
+++ exercise-2.js
@@ -3,7 +3,30 @@ Module Two Exercise: Object Methods
 Start with exercise-2.md if you haven't read it! 
 */
 
-//  DO NOT EDIT THE BELOW OBJECT // 
+//  DO NOT EDIT THE BELOW OBJECT //
+
+// const papaJohns = {
+//   name: "Papa John's",
+//   slogan: "Better Ingredients. Better Pizza. Papa John's.",
+//   cuisines: ["italian", "american", "pizza"],
+//   pizzaToppings: {
+//     pepperoni: 2,
+//     peppers: 0.6,
+//     extraCheese: 1.5,
+//     olives: 0.5,
+//     bacon: 3,
+//     extraSauce: 1,
+//   },
+//   numberOfStars: 3.5,
+//   favorited: false,
+//   address: "555 Main Street",
+//   zipcode: 11234,
+//   acceptsReservations: false,
+// };
+
+// DO NOT EDIT THE ABOVE OBJECT //
+
+// YOUR WORK GOES HERE //
 
 const papaJohns = {
   name: "Papa John's",
@@ -23,7 +46,75 @@ const papaJohns = {
   zipcode: 11234,
   acceptsReservations: false,
 };
+const newAddress = "666 main drive";
+const otherSlogan = "come get the shap at papashaqs";
 
-// DO NOT EDIT THE ABOVE OBJECT // 
+papaJohns.grabCategories = function (papaJohns) {
+  return Object.keys(papaJohns);
+};
 
-// YOUR WORK GOES HERE // 
+function verifyValues(obj, val) {
+  if (Object.values(obj).includes(val)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getToppingsInfo(obj) {
+  return Object.entries(obj.pizzaToppings);
+}
+
+papaJohns.printad = function (pizzaToppings, price) {
+  return (
+    "Welcome to Papa John's! We are located at 555 Main Street ,This week, we are having a sale on " +
+    pizzaToppings +
+    " for " +
+    price +
+    "$. Better Ingredients. Better Pizza. Papa John's."
+  );
+};
+
+// Object.prototype.printad = function () {
+//   return (
+//     "Welcome to Papa Johns! We are located at " +
+//     this.address +
+//     ", This week, we are having a sale on " +
+//     Object.keys(this.pizzaToppings).join(", ") +
+//     " for " +
+//     Object.values(this.pizzaToppings).join(" and ") +
+//     "$. " +
+//     this.slogan
+//   );
+
+papaJohns.ad = function (address, pizzaToppings, price, slogan) {
+  return (
+    "Welcome to Papa Johns! We are located at " +
+    address +
+    ", This week, we are having a sale on " +
+    pizzaToppings +
+    " for " +
+    price +
+    "$. " +
+    slogan
+  );
+};
+
+console.log(
+  papaJohns.ad(
+    "666 main drive",
+    "extraCheese",
+    1,
+    "come get the shap at papashaqs"
+  )
+);
+
+console.log(papaJohns.printad("bacon", 3));
+
+console.log(getToppingsInfo(papaJohns));
+
+console.log(verifyValues(papaJohns, 88989));
+console.log(verifyValues(papaJohns, 3.5));
+
+console.log(papaJohns.grabCategories(papaJohns));
+console.log(papaJohns.grabCategories(papaJohns.pizzaToppings));
